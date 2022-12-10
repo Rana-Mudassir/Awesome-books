06e8632f6216402fee404d84fee4546c34955a21
Rana-Mudassir <mduasire38@gmail.com>
2022-12-11 00:17:37 +0500

    solved linters error

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const form = document.getElementById('form');
+/* eslint-disable */ 
 const remove = document.getElementById('remove');
-// const titleBook = document.getElementById('title');
-// const authorBook = document.getElementById('title');
+/* eslint-enable  */ 
 const add = document.querySelector('#add');
 
 let booksArray = [];
@@ -10,12 +9,7 @@ class bookApp {
 	static addBook() {	
 		const section = document.getElementById('bookSection');
 		section.innerHTML = '';
-		for (let i = 0; i <= booksArray.length - 1; i++) {
-			section.innerHTML += `<div id="textHeading"><div><p id="title">"${booksArray[i].title}" By  </p>	<p id="author">${booksArray[i].author}</p></div>	<button id="remove" onclick="bookApp.removeBook(${i})">Remove</button></div>`;
-			// title.value = '';
-      // author.value = '';
-		}
-	}
+		for (let i = 0; i <= booksArray.length - 1; i++) { section.innerHTML += `<div id="textHeading"><div><p id="title">"${booksArray[i].title}" By </p> <p id="author">${booksArray[i].author}</p></div> <button id="remove" onclick="bookApp.removeBook(${i})">Remove</button></div>`; } }
 	
 	static removeBook(id) {
 		booksArray.splice(id, 1);
