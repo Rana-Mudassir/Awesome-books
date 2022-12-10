b90e5c6e6d8fa3b4a63ab220c96a966502bbbe02
Rana-Mudassir <mduasire38@gmail.com>
2022-12-11 01:23:07 +0500

    solved linters error

--- script.js
+++ script.js
@@ -7,14 +7,35 @@ let booksArray = [];
 
 /* eslint-disable */ 
 class bookApp {
+
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
   static addBook() {
+    const title = document.getElementById('inputTitle');
+    const author = document.getElementById('inputAuthor');
+    const newBook = {
+      title: title.value,
+      author: author.value,
+    };
+    booksArray.push(newBook);
+    bookApp.displayBook();
+    localStorage.setItem('booksArray', JSON.stringify(booksArray));
+    title.value = '';
+    author.value = '';
+    // debugger;
+  }
+
+  static displayBook() {
     const section = document.getElementById('bookSection');
     section.innerHTML = '';
     for (let i = 0; i <= booksArray.length - 1; i++) { section.innerHTML += `<div id="textHeading"><div><p id="title">"${booksArray[i].title}" By </p> <p id="author">${booksArray[i].author}</p></div> <button id="remove" onclick="bookApp.removeBook(${i})">Remove</button></div>`; }
   }
   static removeBook(id) {
     booksArray.splice(id, 1);
-    bookApp.addBook();
+    bookApp.displayBook();
     localStorage.setItem('booksArray', JSON.stringify(booksArray));
   }
 }
@@ -25,21 +46,9 @@ window.onload = () => {
   if (localStorage.getItem('booksArray')) {
     booksArray = JSON.parse(localStorage.getItem('booksArray'));
   }
-  bookApp.addBook();
+  bookApp.displayBook();
 };
 
 if (add) {
-  add.addEventListener('click', () => {
-    const title = document.getElementById('inputTitle');
-    const author = document.getElementById('inputAuthor');
-    const newBook = {
-      title: title.value,
-      author: author.value,
-    };
-    booksArray.push(newBook);
-    bookApp.addBook();
-    localStorage.setItem('booksArray', JSON.stringify(booksArray));
-    title.value = '';
-    author.value = '';
-  });
+  add.addEventListener('click', bookApp.addBook);
 }
