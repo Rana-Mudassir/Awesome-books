91863effb1f6ee69ca3b5e76686ca363bf5cd17e
Rana-Mudassir <mduasire38@gmail.com>
2022-12-10 23:30:30 +0500

    Updated local storage functnality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint-disable rule-you-want-to-disable */
 const form = document.getElementById('form');
 const remove = document.getElementById('remove');
 const titleBook = document.getElementById('title');
@@ -7,39 +8,39 @@ const add = document.querySelector('#add');
 let booksArray = [];
 var books = {};
 function addBook() {
-	const inputTitle = document.getElementById('inputTitle');
-	const inputAuthor = document.getElementById('inputAuthor');
-	var title = inputTitle.value;
-	var author = inputAuthor.value;
-	
-	booksArray.push({
-		id: Date.now(),
-		title: title,
-		author: author
-	})
-	console.log('booksArray', booksArray)
-	displayBooksList()
-}
-
-// Display Function
-function displayBooksList() {
-	
-	const section = document.getElementById('bookSection');
-	section.innerHTML = '';
-	for (let i = 0; i <= booksArray.length - 1; i++) {
-		section.innerHTML += `<p id="title">${booksArray[i].title}</p>	<p id="author">${booksArray[i].author}</p>	<button id="remove" onclick="removeBook(${booksArray[i].id})">Remove</button>
-		<hr />`;
-	}
+  const section = document.getElementById('bookSection');
+  section.innerHTML = '';
+  for (let i = 0; i <= booksArray.length - 1; i++) {
+    section.innerHTML += `<div id="textHeading"><div><p id="title">"${booksArray[i].title}" By  </p>	<p id="author">${booksArray[i].author}</p></div>	<button id="remove" onclick="removeBook(${i})">Remove</button></div> <hr />`;
+  }
 }
 
 function removeBook(id) {
-	console.log('array 1', booksArray)
-	booksArray = booksArray.filter((obj) => obj.id !== id);
-	console.log('array 2', booksArray)
-	displayBooksList();
+  booksArray.splice(id, 1);
+  addBook();
+  localStorage.setItem('booksArray', JSON.stringify(booksArray));
 }
+
+// Local Storage
+window.onload = () => {
+  if (localStorage.getItem('booksArray')) {
+    booksArray = JSON.parse(localStorage.getItem('booksArray'));
+  }
+  addBook();
+};
 
 if (add) {
-	add.addEventListener('click', addBook)
+  add.addEventListener('click', () => {
+    const title = document.getElementById('inputTitle');
+    const author = document.getElementById('inputAuthor');
+    const newBook = {
+      title: title.value,
+      author: author.value,
+    };
+    booksArray.push(newBook);
+    addBook();
+    localStorage.setItem('booksArray', JSON.stringify(booksArray));
+    title.value = '';
+    author.value = '';
+  });
 }
-
