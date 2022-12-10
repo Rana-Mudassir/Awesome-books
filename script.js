ad82aebaffbb8655b9fc3624a19dd90b77bc5b7b
Rana-Mudassir <mduasire38@gmail.com>
2022-12-11 01:22:42 +0500

    solved linters error

--- script.js
+++ script.js
@@ -1,64 +1,54 @@
 /* eslint-disable */
 const remove = document.getElementById('remove');
-/* eslint-enable */
+/* eslint-enable  */
 const add = document.querySelector('#add');
 
 let booksArray = [];
-function addBook() {
-  const inputTitle = document.getElementById('inputTitle');
-  const inputAuthor = document.getElementById('inputAuthor');
-  var title = inputTitle.value;
-  var author = inputAuthor.value;
 
-  booksArray.push({
-    id: Date.now(),
-    title: title,
-    author: author,
-  });
-  displayBooksList();
-}
+/* eslint-disable */ 
+class bookApp {
 
-function displayBooksList() {
-  const section = document.getElementById('bookSection');
-  section.innerHTML = '';
-  for (let i = 0; i <= booksArray.length - 1; i++) {
-    section.innerHTML += `<div id="textHeading"><div><p id="title">"${booksArray[i].title}" By </p> <p id="author">${booksArray[i].author}</p></div> <button id="remove" onclick="removeBook(${booksArray[i].id})">Remove</button></div>`;
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  static addBook() {
+    const title = document.getElementById('inputTitle');
+    const author = document.getElementById('inputAuthor');
+    const newBook = {
+      title: title.value,
+      author: author.value,
+    };
+    booksArray.push(newBook);
+    bookApp.displayBook();
+    localStorage.setItem('booksArray', JSON.stringify(booksArray));
+    title.value = '';
+    author.value = '';
+    // debugger;
+  }
+
+  static displayBook() {
+    const section = document.getElementById('bookSection');
+    section.innerHTML = '';
+    for (let i = 0; i <= booksArray.length - 1; i++) { section.innerHTML += `<div id="textHeading"><div><p id="title">"${booksArray[i].title}" By </p> <p id="author">${booksArray[i].author}</p></div> <button id="remove" onclick="bookApp.removeBook(${i})">Remove</button></div>`; }
+  }
+  static removeBook(id) {
+    booksArray.splice(id, 1);
+    bookApp.displayBook();
+    localStorage.setItem('booksArray', JSON.stringify(booksArray));
   }
 }
-
-function removeBook(id) {
-  booksArray = booksArray.filter((obj) => obj.id !== id);
-  displayBooksList();
-}
-
-if (add) {
-  add.addEventListener('click', addBook);
-}
+/* eslint-enable  */
 
 // Local Storage
-
-const title = document.getElementById('inputTitle');
-const author = document.getElementById('inputAuthor');
-
-function getFormInfo() {
-  const formData = {
-    title: title.value,
-    author: author.value,
-  };
-  localStorage.setItem('formData', JSON.stringify(formData));
-}
-
-function storeData() {
-  if (!localStorage.getItem('formData')) {
-    getFormInfo();
-  } else {
-    const fetchData = JSON.parse(localStorage.getItem('formData'));
-    title.setAttribute('value', fetchData.title);
-    author.setAttribute('value', fetchData.author);
-  }
-}
 window.onload = () => {
-  storeData();
+  if (localStorage.getItem('booksArray')) {
+    booksArray = JSON.parse(localStorage.getItem('booksArray'));
+  }
+  bookApp.displayBook();
 };
-title.addEventListener('change', getFormInfo);
-author.addEventListener('change', getFormInfo);
+
+if (add) {
+  add.addEventListener('click', bookApp.addBook);
+}
