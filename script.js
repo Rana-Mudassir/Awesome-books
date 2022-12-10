5c671e5f1ead40594bcaa75c9cdae918fec1e71d
Rana-Mudassir <mduasire38@gmail.com>
2022-12-11 00:34:20 +0500

    solved linters error

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-/* eslint-disable */ 
+/* eslint-disable */
 const remove = document.getElementById('remove');
-/* eslint-enable  */ 
+/* eslint-enable  */
 const add = document.querySelector('#add');
 
 let booksArray = [];
 
+/* eslint-disable */ 
 class bookApp {
-	static addBook() {	
-		const section = document.getElementById('bookSection');
-		section.innerHTML = '';
-		for (let i = 0; i <= booksArray.length - 1; i++) { section.innerHTML += `<div id="textHeading"><div><p id="title">"${booksArray[i].title}" By </p> <p id="author">${booksArray[i].author}</p></div> <button id="remove" onclick="bookApp.removeBook(${i})">Remove</button></div>`; } }
-	
-	static removeBook(id) {
-		booksArray.splice(id, 1);
-		bookApp.addBook();
-		localStorage.setItem('booksArray', JSON.stringify(booksArray));
-	}
+  static addBook() {
+    const section = document.getElementById('bookSection');
+    section.innerHTML = '';
+    for (let i = 0; i <= booksArray.length - 1; i++) { section.innerHTML += `<div id="textHeading"><div><p id="title">"${booksArray[i].title}" By </p> <p id="author">${booksArray[i].author}</p></div> <button id="remove" onclick="bookApp.removeBook(${i})">Remove</button></div>`; }
+  }
+  static removeBook(id) {
+    booksArray.splice(id, 1);
+    bookApp.addBook();
+    localStorage.setItem('booksArray', JSON.stringify(booksArray));
+  }
 }
+/* eslint-enable  */ 
 
 // Local Storage
 window.onload = () => {
   if (localStorage.getItem('booksArray')) {
-		booksArray = JSON.parse(localStorage.getItem('booksArray'));
+    booksArray = JSON.parse(localStorage.getItem('booksArray'));
   }
-	bookApp.addBook();
+  bookApp.addBook();
 };
 
 if (add) {
